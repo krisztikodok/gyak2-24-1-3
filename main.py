a80a03ac3ffe8c493c0f390c094258df5e23004c
krisztikodok <135458893+krisztikodok@users.noreply.github.com>
2024-01-03 18:36:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+
+
+"""
+OS stuff
+import os
+
+print(os.getcwd())
+print(os.getcwdb())
+print(os.get_terminal_size())
+print(os.listdir())
+
+#os.mkdir("Test2")
+#os.rename("Test2","Misc")
+
+#os.remove("Test/1.txt")
+
+#os.rmdir("Test")
+os.chdir("Misc")
+os.mkdir("A")
+os.mkdir("B")
+"""
